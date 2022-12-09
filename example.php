a51922a5d541c07b78691c4b4e4932ed9b7b422b
Heisue Wang <whs@bandibiz.com>
2022-12-09 18:00:23 +0900

    091222

---
+++ example.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title></title>
+</head>
+<body>
+
+<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+    $(document).ready(function(){
+        $("<h1>").text("hello").append("body");
+    })
+    </script>
+</body>
+</html>
